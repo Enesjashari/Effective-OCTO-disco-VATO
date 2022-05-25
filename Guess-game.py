f4d4c31203f9af0a907b48e5f80e003fb0d937ae
Enesjashari <93095569+Enesjashari@users.noreply.github.com>
2022-05-25 11:37:19 -1100

    Create Guess-game.py

---
+++ Guess-game.py
@@ -0,0 +1,10 @@
+guess = 18
+a = int(input('enter a numbers: '))
+
+if a > guess:
+    print('the number you enterd is big')
+elif a < guess:
+    print('the number you enterd is small')
+else:
+    print('you are correct the number is 18')
+
